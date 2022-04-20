252195405761b2e7816cd545adcb9f984c8f0fc9
jwilll4455 <burakceylann@outlook.com>
2022-04-20 10:40:19 +0300

    Create viewmodel for services

---
+++ lib/viewmodel/news_viewmodel.dart
@@ -0,0 +1,24 @@
+import 'dart:convert';
+
+import 'package:get/get.dart';
+import 'package:http/http.dart' as http;
+
+import '../model/news_model.dart';
+
+class NewsViewModel extends GetxController {
+  getData() async {
+    final url = Uri.parse(
+        'https://newsapi.org/v2/top-headlines?country=tr&apiKey=656195782efe42c4a6111ad1b2c0a00c'
+    );
+    http.Response response = await http.get(url);
+    if(response.statusCode ==200){
+      try{
+        return NewsModel.fromJson(jsonDecode(response.body));
+      }
+      catch(e){
+          Get.snackbar('can not get any data', e.toString());
+      }
+    }
+  }
+
+}
