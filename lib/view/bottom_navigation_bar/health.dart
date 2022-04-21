c9f91c22284a7ef6d69f7fd6f446d3d29943bf99
jwilll4455 <burakceylann@outlook.com>
2022-04-21 09:06:53 +0300

    Add health news screen

---
+++ lib/view/bottom_navigation_bar/health.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:news_app_flutter_mvvm/viewmodel/news_viewmodel.dart';
+
+import '../../model/news_model.dart';
+
+class Health extends GetWidget<NewsViewModel> {
+  @override
+  final controller = Get.put(NewsViewModel());
+
+  Health({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+          future: controller.getData('health'),
+          builder: (context, AsyncSnapshot snapshot) {
+            NewsModel? data = snapshot.data;
+            if (snapshot.hasData) {
+              return ListView.builder(
+                  itemCount: data!.articles!.length,
+                  itemBuilder: (context, index) {
+                    return Column(
+                      children: [
+                        Image.network(
+                          data.articles![index].urlToImage.toString(),
+                          fit: BoxFit.fill,
+                        ),
+                        Text(
+                          data.articles![index].title.toString(),
+                          style: const TextStyle(
+                              fontSize: 20, fontWeight: FontWeight.bold),
+                        ),
+                        Text(
+                          data.articles![index].description.toString(),
+                          style: const TextStyle(
+                              fontSize: 16, fontWeight: FontWeight.bold, color: Colors.grey),
+                        ),
+                      ],
+                    );
+                  });
+            } else {
+              return const Center(child: CircularProgressIndicator());
+            }
+          },
+    );
+  }
+}
