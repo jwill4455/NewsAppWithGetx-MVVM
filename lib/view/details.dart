c3bd623d8f0d2a2c316110fe0c473167e98fee92
jwilll4455 <burakceylann@outlook.com>
2022-04-21 16:10:17 +0300

    Add column for details

--- lib/view/details.dart
+++ lib/view/details.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
 import 'package:news_app_flutter_mvvm/model/news_model.dart';
-import 'package:news_app_flutter_mvvm/viewmodel/news_viewmodel.dart';
 
 
 class NewsDetail extends StatelessWidget {
@@ -13,7 +11,20 @@ class NewsDetail extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(articles.title!),
+        title: Text(articles.title.toString()),
+      ),
+      body: Column(
+        children: [
+          Image.network(
+            articles.urlToImage.toString(),
+            fit: BoxFit.fill,
+          ),
+          Text(
+            articles.description.toString(),
+            style: const TextStyle(
+                fontSize: 16, fontWeight: FontWeight.bold, color: Colors.grey),
+          ),
+        ],
       ),
     );
   }
