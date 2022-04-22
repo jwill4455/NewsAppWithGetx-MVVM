a0e80c77ec77330182d4d7a6e368cd344e4f745a
jwilll4455 <burakceylann@outlook.com>
2022-04-22 13:33:10 +0300

    Some design adjusments on main view

--- lib/view/view.dart
+++ lib/view/view.dart
@@ -2,13 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:news_app_flutter_mvvm/view/bottom_navigation_bar/health.dart';
 import 'package:news_app_flutter_mvvm/view/bottom_navigation_bar/sports.dart';
+import 'package:news_app_flutter_mvvm/view/search.dart';
 import 'package:news_app_flutter_mvvm/viewmodel/services/news_viewmodel.dart';
 
 import 'bottom_navigation_bar/favorites.dart';
 import 'bottom_navigation_bar/science.dart';
 
 class HomeView extends StatefulWidget {
-
   const HomeView({Key? key}) : super(key: key);
 
   @override
@@ -20,72 +20,69 @@ class _HomeViewState extends State<HomeView> {
 
   int _currentIndex = 0;
 
-  List<Widget> screens=[
-    Sports(),
-    Science(),
-    Health(),
-    Favorites()
-  ];
-
-
+  List<Widget> screens = [Sports(), Science(), Health(), Favorites()];
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        backgroundColor: Colors.black,
-        title: const Text.rich(
-          TextSpan(
-            children: [
+
+        appBar: AppBar(
+          title: const Text.rich(
               TextSpan(
-                text: 'News', style: TextStyle(
-                fontSize: 25, fontWeight: FontWeight.bold, color: Colors.orange
-              ),
-              ),
-              TextSpan(
-                text: 'App', style: TextStyle(
-                  fontSize: 20, fontWeight: FontWeight.bold, color: Colors.white
-              ),
+                  children: [
+                    TextSpan(
+                      text: 'News', style: TextStyle(
+                        fontSize: 25, fontWeight: FontWeight.bold, color: Colors.orange
+                    ),
+                    ),
+                    TextSpan(
+                      text: 'App', style: TextStyle(
+                        fontSize: 20, fontWeight: FontWeight.bold, color: Colors.white
+                    ),
+                    )
+                  ]
               )
-            ]
-          )
+          ),
+          centerTitle: true,
+          actions: [
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16),
+              child: IconButton(icon: const Icon(Icons.search),
+              onPressed: (){
+                showSearch(context: context, delegate: SearchUser());
+              }
+              ),
+            ),
+          ],
+          backgroundColor: Colors.black,
         ),
-      ),
 
-      bottomNavigationBar: BottomNavigationBar(
-        showUnselectedLabels: true,
-        unselectedItemColor: Colors.grey,
-        selectedItemColor: Colors.orange,
-        currentIndex: _currentIndex,
-        onTap: (val){
-          setState(() {
-            _currentIndex = val;
-          });
 
-        },
-        items: const [
-          BottomNavigationBarItem(
-            label: 'Sports',
-              icon: Icon(Icons.sports_football)
-          ),
-          BottomNavigationBarItem(
-              label: 'Science',
-              icon: Icon(Icons.science)
-          ),
-          BottomNavigationBarItem(
-              label: 'Health',
-              icon: Icon(Icons.health_and_safety)
-          ),
-          BottomNavigationBarItem(
-              label: 'Favorites',
-              icon: Icon(Icons.favorite)
-          )
-        ],
-      ),
-        body: screens[_currentIndex]
-    );
-
+        bottomNavigationBar: BottomNavigationBar(
+          showUnselectedLabels: true,
+          unselectedItemColor: Colors.grey,
+          selectedItemColor: Colors.orange,
+          currentIndex: _currentIndex,
+          onTap: (val) {
+            setState(() {
+              _currentIndex = val;
+            });
+          },
+          items: const [
+            BottomNavigationBarItem(
+                label: 'Sports', icon: Icon(Icons.sports_basketball)
+            ),
+            BottomNavigationBarItem(
+                label: 'Science', icon: Icon(Icons.science)
+            ),
+            BottomNavigationBarItem(
+                label: 'Health', icon: Icon(Icons.health_and_safety)
+            ),
+            BottomNavigationBarItem(
+                label: 'Favorites', icon: Icon(Icons.favorite)
+            )
+          ],
+        ),
+        body: screens[_currentIndex]);
   }
 }
