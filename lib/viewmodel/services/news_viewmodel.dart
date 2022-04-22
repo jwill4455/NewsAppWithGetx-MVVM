779eb038e0647f979a8c4ff44845f0ef2a81c965
jwilll4455 <burakceylann@outlook.com>
2022-04-22 13:32:09 +0300

    Add search link for searching lists

--- lib/viewmodel/services/news_viewmodel.dart
+++ lib/viewmodel/services/news_viewmodel.dart
@@ -10,7 +10,7 @@ class NewsViewModel extends GetxController {
   static String baseUrl = 'https://newsapi.org';
   static String myApiKey = '656195782efe42c4a6111ad1b2c0a00c';
 
-  getData(String category) async {
+  getData(String? category) async {
     final url = Uri.parse(
         '$baseUrl/v2/top-headlines?country=tr&category=$category&apiKey=$myApiKey'
     );
@@ -24,18 +24,24 @@ class NewsViewModel extends GetxController {
       }
     }
   }
-  searchData(String topic) async {
+  List<Articles> results = [];
+  searchData(String? query) async {
     final url = Uri.parse(
-        '$baseUrl/v2/everything/?q=$topic&apiKey=$myApiKey'
+        '$baseUrl/v2/everything/?q=$query&apiKey=$myApiKey'
     );
     http.Response response = await http.get(url);
     if (response.statusCode == 200) {
+
+      if(query != null){
+        results = results.where((element) => element.title!.toLowerCase().contains(query.toLowerCase())).toList();
+      }
       try {
         return NewsModel.fromJson(jsonDecode(response.body));
       }
       catch (e) {
         Get.snackbar('can not get any data', e.toString());
       }
+
     }
   }
 }
