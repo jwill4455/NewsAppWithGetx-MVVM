2860815f47c455b9bb422da520b32ab099bab949
jwilll4455 <burakceylann@outlook.com>
2022-04-26 15:49:53 +0300

    Migrate firebase from details to favorites_firabase page

--- lib/view/details.dart
+++ lib/view/details.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:news_app_flutter_mvvm/firebase/favorites_firebase.dart';
 
 import '../model/article_model.dart';
 
@@ -14,21 +15,12 @@ class NewsDetail extends StatefulWidget {
 }
 
 class _NewsDetailState extends State<NewsDetail> {
-  Future addToFavorite() async {
-    final FirebaseAuth _auth = FirebaseAuth.instance;
-    CollectionReference _collectionRef =
-        FirebaseFirestore.instance.collection("users-favourite-items");
 
-    return _collectionRef.add({
-      "title": widget.articles.title,
-      "description": widget.articles.description,
-      "url": widget.articles.url,
-      "urlToImage": widget.articles.urlToImage,
-    }).then((value) => print("Added to favorite"));
-  }
+  final FavoritesFirebase _favorites = Get.put(FavoritesFirebase());
 
   @override
   Widget build(BuildContext context) {
+
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.articles.title.toString()),
@@ -46,7 +38,12 @@ class _NewsDetailState extends State<NewsDetail> {
                 padding: const EdgeInsets.symmetric(horizontal: 16),
                 child: IconButton(
                   onPressed: () => snapshot.data.docs.length == 0
-                      ? addToFavorite()
+                      ? _favorites.addToFavorite(
+                      widget.articles.title.toString(),
+                      widget.articles.description.toString(),
+                      widget.articles.url.toString(),
+                      widget.articles.urlToImage.toString(),
+                  )
                       :print("Already Added"),
                   icon: snapshot.data.docs.length == 0
                       ? const Icon(
