84263c6e81bd84f8ddede1600d077b7a45b53cc6
jwilll4455 <burakceylann@outlook.com>
2022-04-26 15:50:34 +0300

    Add favorites screen for favorite news

--- lib/view/bottom_navigation_bar/favorites.dart
+++ lib/view/bottom_navigation_bar/favorites.dart
@@ -1,10 +1,49 @@
-import 'package:flutter/cupertino.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
 
-class Favorites extends StatelessWidget {
-   Favorites({Key? key}) : super(key: key);
+class Favorites extends StatefulWidget {
+  const Favorites({Key? key}) : super(key: key);
 
   @override
+  State<Favorites> createState() => _FavoritesState();
+}
+
+class _FavoritesState extends State<Favorites> {
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: SafeArea(
+          child: StreamBuilder(
+        stream: FirebaseFirestore.instance
+            .collection("users-favourite-items")
+            .snapshots(),
+        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+          if (snapshot.hasError) {
+            return const Center(
+              child: Text("Something is wrong"),
+            );
+          }
+          return ListView.builder(
+              itemCount: snapshot.data!.docs.length,
+              itemBuilder: (_, index) {
+                DocumentSnapshot _documentSnapshot = snapshot.data!.docs[index];
+                return Card(
+                  elevation: 5,
+                  child: ListTile(
+                    leading: Text(_documentSnapshot["title"]),
+                    trailing: GestureDetector(
+                      child: const CircleAvatar(
+                        child:  Icon(Icons.remove_circle),
+                      ),
+                      onTap: (){
+                        FirebaseFirestore.instance.collection("users-favourite-items").doc(_documentSnapshot.id).delete();
+                      },
+                    ),
+                  ),
+                );
+              });
+        },
+      )),
+    );
   }
 }
