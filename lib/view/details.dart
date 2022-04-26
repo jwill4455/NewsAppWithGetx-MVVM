1e30ddfc3820573971d9a6a62972a59364afd674
jwilll4455 <burakceylann@outlook.com>
2022-04-26 10:14:56 +0300

    Add firebase integration for favorite button in detail page

--- lib/view/details.dart
+++ lib/view/details.dart
@@ -4,10 +4,7 @@ import 'package:flutter/material.dart';
 
 import '../model/article_model.dart';
 
-
-
 class NewsDetail extends StatefulWidget {
-
   final Article articles;
 
   NewsDetail({required this.articles});
@@ -17,14 +14,18 @@ class NewsDetail extends StatefulWidget {
 }
 
 class _NewsDetailState extends State<NewsDetail> {
-
   Future addToFavorite() async {
     final FirebaseAuth _auth = FirebaseAuth.instance;
-    var currentUser = _auth.currentUser;
     CollectionReference _collectionRef =
-    FirebaseFirestore.instance.collection("users-favourite-items");
-  }
+        FirebaseFirestore.instance.collection("users-favourite-items");
 
+    return _collectionRef.add({
+      "title": widget.articles.title,
+      "description": widget.articles.description,
+      "url": widget.articles.url,
+      "urlToImage": widget.articles.urlToImage,
+    }).then((value) => print("Added to favorite"));
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -32,16 +33,33 @@ class _NewsDetailState extends State<NewsDetail> {
       appBar: AppBar(
         title: Text(widget.articles.title.toString()),
         actions: [
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 16),
-            child: IconButton(
-              icon: const Icon(
-                Icons.favorite,
-                color: Colors.red,
-              ), onPressed: () {
-
+          StreamBuilder(
+            stream: FirebaseFirestore.instance
+                .collection("users-favourite-items")
+                .where("title", isEqualTo: widget.articles.title)
+                .snapshots(),
+            builder: (BuildContext context, AsyncSnapshot snapshot) {
+              if (snapshot.data == null) {
+                return const Text("");
+              }
+              return Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16),
+                child: IconButton(
+                  onPressed: () => snapshot.data.docs.length == 0
+                      ? addToFavorite()
+                      :print("Already Added"),
+                  icon: snapshot.data.docs.length == 0
+                      ? const Icon(
+                          Icons.favorite,
+                          color: Colors.white,
+                        )
+                      : const Icon(
+                          Icons.favorite,
+                          color: Colors.red,
+                        ),
+                ),
+              );
             },
-            ),
           ),
         ],
       ),
@@ -61,4 +79,3 @@ class _NewsDetailState extends State<NewsDetail> {
     );
   }
 }
-
